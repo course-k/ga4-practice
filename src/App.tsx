25327059e05187e6894dc1199c151c2fc462d124
practice_user <practice@mail.co.jp>
2024-06-24 22:39:32 +0900

    modify gtag

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
 
 function App() {
+  const handleClick = () => {
+    window.gtag("event", "child_click", {
+      event_category: "navigation",
+      event_label: "Child Page Link",
+    });
+  };
   return (
     <>
-      <Link to={"/child"} onClick={() => gtag("event", "child_click")}>
+      <Link to={"/child"} onClick={handleClick}>
         child
       </Link>
     </>
