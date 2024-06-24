61afc2483b2dfc2005534be240476634ba3a5090
practice_user <practice@mail.co.jp>
2024-06-24 22:43:11 +0900

    modify gtag

--- src/MyRoutes.tsx
+++ src/MyRoutes.tsx
@@ -2,17 +2,35 @@ import {
   RouteObject,
   RouterProvider,
   createBrowserRouter,
+  useLocation,
 } from "react-router-dom";
 import App from "./App";
 import Child from "./Child";
+import { useEffect } from "react";
 const routes: RouteObject[] = [
   { path: "/", element: <App /> },
   { path: "/child", element: <Child /> },
 ];
 
+function PageViewTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.gtag("event", "page_view", {
+      page_path: location.pathname + location.search,
+    });
+  }, [location]);
+
+  return null;
+}
 const router = createBrowserRouter(routes);
 const MyRoutes = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <PageViewTracker />
+      <RouterProvider router={router} />;
+    </>
+  );
 };
 
 export default MyRoutes;
